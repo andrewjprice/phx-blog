5e4ce566efb35c2a385c68ad9fa39cf4fbb6afc1
Andrew <andrew.j.price@outlook.com>
2019-03-06 23:52:48 -0700

    Modify Post template card

--- lib/phx_blog_web/views/page_view.ex
+++ lib/phx_blog_web/views/page_view.ex
@@ -2,12 +2,19 @@ defmodule PhxBlogWeb.PageView do
   use PhxBlogWeb, :view
 
   def post_excerpt(post) do
-    String.slice(post.body, 0..90) <> " ..."
+    String.slice(post.body, 0..120) <> " ..."
+    |> Earmark.as_html!
+    |> raw
   end
 
   def markdown(body) do
     body
     |> Earmark.as_html!
     |> raw
+  end
+
+  def relative_date(post) do
+    {:ok, relative_str} = Timex.format(post.inserted_at, "{relative}", :relative)
+    relative_str
   end
 end
